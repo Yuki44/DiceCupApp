552fc0570dc1dc581c5d23ea517c9abdde20cf5a
moystre <moystre@gmail.com>
2018-03-01 18:33:54 +0100

    clearing history & going back
    using buttons

--- app/src/main/java/com/easv/yuki/dicecupapp/HistoryActivity.java
+++ app/src/main/java/com/easv/yuki/dicecupapp/HistoryActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -12,12 +14,28 @@ public class HistoryActivity extends AppCompatActivity {
     ArrayList<BERoll> diceList;
     private RecyclerView mDiceList;
     private RollListAdapter rollListAdapter;
-
+    private Button clearButton;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        clearButton = findViewById(R.id.clearButton);
+        backButton = findViewById(R.id.backButton);
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mDiceList.setAdapter(null);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         diceList = (ArrayList<BERoll>) getIntent().getSerializableExtra("dicelist");
         rollListAdapter = new RollListAdapter(diceList);
